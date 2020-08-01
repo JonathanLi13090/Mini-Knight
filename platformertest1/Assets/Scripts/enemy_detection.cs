9057c5b32e081151617f73a6833cbbc1be20b8e6
JonathanLi13090 <65463483+JonathanLi13090@users.noreply.github.com>
2020-08-01 11:05:01 -0700

    8/1/2020
    sort of invincibility timer

--- platformertest1/Assets/Scripts/enemy_detection.cs
+++ platformertest1/Assets/Scripts/enemy_detection.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class enemy_detection : MonoBehaviour
 {
     public float health = 4f;
-
+    public List<Animator> HealthBubbles;
+    public float invincibilityTime = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetBubbles();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -29,9 +30,26 @@ public class enemy_detection : MonoBehaviour
             else
             {
                 health = health -= 1;
+                SetBubbles();
                 Debug.Log(health);
+                for (int i = 0; i < 10; i += 1)
+                {
+                    invincibilityTime -= Time.deltaTime;
+                    if (invincibilityTime < 0)
+                    {
+                        break;
+                    }
+                }
             }
 
+        }
+    }
+    private void SetBubbles()
+    {
+        for (int i = 0; i < HealthBubbles.Count; i += 1)
+        {
+            if (i > health - 1) HealthBubbles[i].SetBool("Full", false);
+            else HealthBubbles[i].SetBool("Full", true);
         }
     }
 }
